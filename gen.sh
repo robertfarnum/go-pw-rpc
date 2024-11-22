921068f8e5f8c5c5b5b6b30a27871680031a5f16
Robert Farnum <robertfarnum@gmail.com>
2024-11-22 11:49:07 -0600

    checkpoint

--- gen.sh
+++
@@ -1,8 +0,0 @@
-protoc --go_out=. ./packet.proto
-protoc --go_out=. ./benchmark.proto
-protoc \
-    --go_out=. \
-    --go_opt=Munit_test.proto=./pb \
-    --go-grpc_out=. \
-    --go-grpc_opt=Munit_test.proto=./pb \
-    ./unit_test.proto
